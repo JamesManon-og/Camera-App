400099f7ef8f94fff2bccdb8b7746d79b9c0525f
James Manon-og <jamesmanon-og@Jamess-MacBook-Pro-2.local>
2025-07-14 18:03:29 +0800

    Add: Create CameraScreen component with welcome message and navigation link

---
+++ src/app/image.tsx
@@ -0,0 +1,29 @@
+import { Link } from "expo-router";
+import { View, Text } from "react-native";
+
+export default function CameraScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+        Welcome to the Camera App!
+      </Text>
+      <Text style={{ textAlign: "center", marginTop: 10 }}>
+        Explore the features and functionalities.
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          marginTop: 10,
+          fontStyle: "italic",
+          fontSize: 10,
+        }}
+      >
+        -- this is the image screen --
+      </Text>
+
+      <Link href="/">
+        <Text style={{ color: "blue", marginTop: 10 }}>Go Home</Text>
+      </Link>
+    </View>
+  );
+}
